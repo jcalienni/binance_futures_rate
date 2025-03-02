d31b55fe4ab5c963345f9d431c399233a8fa386d
jcalienni <juliancalienni@gmail.com>
2025-03-02 11:35:14 -0300

    working version

---
+++ binance_futures_rate.bat
@@ -0,0 +1,5 @@
+@echo off
+cd C:\Projects\binance_futures_rate
+call .venv\Scripts\activate.bat
+python main.py
+pause
---
+++ start_sever.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d C:\Projects\binance_futures_rate
+
+:: Start the Python HTTP server in a new minimized window
+start /min cmd /c "python -m http.server 8000"
+
+:: Wait a few seconds to ensure the server starts
+timeout /t 3 /nobreak >nul
+
+:: Open in Google Chrome
+start chrome http://localhost:8000/graph.html
